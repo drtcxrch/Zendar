bd531c638481ae29fdbc94a1c9ce23f04199c1aa
Grant Taggart <granttaggartsf@gmail.com>
2022-02-04 12:26:27 -0800

    Cleanup file structure and fix last logic problem

--- src/Table.js
+++ src/Table.js
@@ -1,16 +1,20 @@
 const Table = (props) => {
     const rowArr = [];
-    console.log(props)
+
     if (props.cols === 0) {
       for (let k = 0; k < props.rows; k++) {
         rowArr.push(<tr key={k} >[{k}, 0]</tr>)
+      }
+    } else if (props.rows === 0 && props.cols > 0) {
+      for (let l = 0; l < props.cols; l++) {
+        rowArr.push(<td key={l} >[0, {l}]</td>)
       }
     } else {
       for (let i = 0; i < props.rows; i++) {
         let colsArr = [];
         let j;
         for (j = 0; j < props.cols; j++) {
-          colsArr.push(<td key={i, j} >[{i}, {j}]</td>)
+          colsArr.push(<td key={i + j} >[{i}, {j}]</td>)
         }
 
         rowArr.push(<tr key={i} >{colsArr}</tr>)
